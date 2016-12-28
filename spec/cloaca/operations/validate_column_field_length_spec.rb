48d66027fe537c32a3839a1d57dafeb49afe5a1e
David Herring <dherring@dherring.com>
2016-12-28 16:06:50 -0600

    add max and min semantics to ValidateColumnFieldLength

--- spec/cloaca/operations/validate_column_field_length_spec.rb
+++ spec/cloaca/operations/validate_column_field_length_spec.rb
@@ -4,15 +4,14 @@ describe Cloaca::Operations::ValidateColumnFieldLength do
 
   let(:output) { StringIO.new }
   let(:result) { output.string }
-  let(:column_index) { 1 }
-  let(:field_length) { 1 }
   let(:result_rows) { result.split("\n") }
 
   def options(overrides = {})
     {
       column_delimiter: "|",
-      column_index: column_index,
-      field_length: field_length,
+      column_index: 1,
+      min_field_length: 1,
+      max_field_length: 5,
       input: input,
       output: output,
     }.merge(overrides)
@@ -21,17 +20,31 @@ describe Cloaca::Operations::ValidateColumnFieldLength do
   context "given rows with fewer columns than column_index" do
     let(:input) { StringIO.new([
       ["A2z", "B2z", "C2z"].join("|"),
-      ["A1z", "B1z", "C1", "D1z"].join("|"),      
+      ["A1zz", "B1z", "C1", "D1z"].join("|"),      
     ].join("\n")) }
 
     it "raises using an explicit row contaning too few tokens" do
-      subject = described_class.new(options.merge(column_delimiter: "|", column_index: 3, field_length: 4))
+      subject = described_class.new(options.merge(column_delimiter: "|", column_index: 3, min_field_length: 4))
       expect { subject.run! }.to raise_exception("not enough tokens on line 0")
     end
 
-    it "raises using an explicit row and column contaning a token too short" do
-      subject = described_class.new(options.merge(column_delimiter: "|", column_index: 2, field_length: 3))
-      expect { subject.run! }.to raise_exception("token in row 1, column 2 has inproper length, not equal to 3")
+    it "raises using an explicit row and column when a toen is too short" do
+      subject = described_class.new(options.merge(column_delimiter: "|", column_index: 2, min_field_length: 3))
+      expect { subject.run! }.to raise_exception("token in row 1, column 2 has inproper length, not >= 3")
+    end
+
+    it "raises using an explicit row and column when a token is too long" do
+      subject = described_class.new(options.merge(column_delimiter: "|", column_index: 0, min_field_length: 2, max_field_length: 3))
+      expect { subject.run! }.to raise_exception("token in row 1, column 0 has inproper length, not <= 3")
+    end
+
+    it "raises using an explicit exception when misconfigured with min and max field lengths" do
+      expect { described_class.new(options.merge(
+          column_delimiter: "|",
+          column_index: 0,
+          min_field_length: 3,
+          max_field_length: 2))
+      }.to raise_exception("configured with inproper min_field_length max_field_length")
     end
   end
 end
